e1745d00107ecad944ac1c6f18ae8b595fd8897a
Jatin Sikka <Jsikka@utexas.edu>
2026-03-06 16:22:25 +0530

    Initial Commit

---
+++ FocusPact/FocusPactApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusPactApp.swift
+//  FocusPact
+//
+//  Created by Aman lucid on 06/03/26.
+//
+
+import SwiftUI
+
+@main
+struct FocusPactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
